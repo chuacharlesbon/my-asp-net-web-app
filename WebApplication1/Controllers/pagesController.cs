ac4aaab2fee1c1dbe772194b6a9c538ae183edf3
Charles Bon Chua <chuacharlesbon@gmail.com>
2024-01-30 18:12:19 +0800

    Initial commit

---
+++ WebApplication1/Controllers/pagesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class pagesController : Controller
+    {
+        private readonly ILogger<pagesController> _logger;
+
+        public pagesController(ILogger<pagesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        public IActionResult privacy()
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult loginstudent()
+        {
+            return View();
+        }
+
+        public IActionResult forgotpassword()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
